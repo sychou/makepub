7202a045388abe9eb7b18651fad6dd2a15627905
Sean Chou <sean@choufam.com>
2024-04-03 08:12:41 -0500

    Manual nav (broken)

--- makepub.py
+++ makepub.py
@@ -1,9 +1,20 @@
+from bs4 import BeautifulSoup
 from datetime import datetime
+from dotenv import load_dotenv
 from ebooklib import epub
+from readability.readability import Document
 import feedparser
 import lxml.etree as ET
+import os
 import requests
 import time
+
+
+load_dotenv()
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+
+OPML_PATH = 'makepub.opml'
+
 
 def read_opml(opml_path):
     data = {'title': None, 'feeds': []}
@@ -23,82 +34,133 @@ def read_opml(opml_path):
 
     return data
 
-def fetch_feed_content(xmlUrl):
-    """Fetches and parses the RSS feed returning title and summary of the first few items."""
-    content = []
-    response = requests.get(xmlUrl)
-    feed = feedparser.parse(response.content)
 
-    for entry in feed.entries[:5]:  # Limit to first 5 entries for brevity
-        content_entry = {
-            'title': entry.title,
-            'link': entry.link,
-            'description': entry.description,
-            'published': entry.published_parsed,
+def fetch_feeds(feeds):
+    """Fetches the feeds and articles."""
+
+    feeds_content = {}
+
+    for i, feed in enumerate(feeds['feeds'], start=1):
+
+        print(f"Fetching {feed['title']}...")
+        response = requests.get(feed['xmlUrl'])
+        articles = []
+        feed_data = feedparser.parse(response.content)
+
+        # TODO Limit to first 3 entries for testing
+        for j, entry in enumerate(feed_data.entries[:3], start=1):
+            article = {
+                'title': entry.title,
+                'link': entry.link,
+                # 'description': entry.description,
+                'published': entry.published_parsed,
+                'index': j,
+                'filename': f'article_{i}_{j}.xhtml',
+            }
+            if 'author' in entry:
+                article['author'] = entry.author
+
+            articles.append(article)
+        feeds_content[feed['title']] = {
+            'articles': articles,
+            'index': i,
+            'filename': f'feed_{i}.xhtml',
         }
-        if 'author' in entry:
-            content_entry['author'] = entry.author
+    return feeds_content
 
-        content.append(content_entry)
-    return content
 
-def create_epub_from_opml(feeds_info, output_path):
+def create_toc(title, articles):
+    """Create a Table of Contents for the EPUB."""
+    # toc = '<nav xmlns:epub="http://www.idpf.org/2007/ops" epub:type="toc">'
+    toc = "<?xml version='1.0' encoding='utf-8'?>\n"
+    toc += '<!DOCTYPE html>\n'
+    toc += '<html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops" lang="en" xml:lang="en">\n'
+    toc += '<head>\n'
+    toc += f"<title>{title}</title>\n"
+    toc += '</head>\n'
+    toc += '<body>\n'
+    toc += '<nav epub:type="toc" id="id" role="doc-toc">\n'
+    toc += '<h2>Table of Contents</h2>\n'
+    toc += '<ol>\n'
+    for feed_title, articles in articles.items():
+        toc += f'<li><a href="{articles["filename"]}">{feed_title}</a></li>\n'
+        toc += '<ol>'
+        for article in articles['articles']:
+            toc += f'<li><a href="{article["filename"]}">{article["title"]}</a></li>\n'
+        toc += '</ol>\n'
+    toc += '</ol>\n'
+    toc += '</nav>\n'
+    toc += '</body>\n'
+    toc += '</html>\n'
+
+    print(toc)
+    return toc
+
+
+def create_epub(feeds, articles):
+    title = feeds['title'] + ' - ' + datetime.now().strftime('%B %-d, %Y')
+
     book = epub.EpubBook()
-    book.set_title(feeds_info['title'])
+    book.set_title(title)
 
-    # Define CSS style for page breaks
-    style = 'body { font-family: Times, serif; margin: 5%; } .page-break { page-break-after: always; }'
-    nav_css = epub.EpubItem(uid="style_nav", file_name="style/style.css", media_type="text/css", content=style)
-    book.add_item(nav_css)
+    # Create a custom nav.xhtml
+    nav_item = epub.EpubItem(
+        uid='nav',
+        file_name='nav.xhtml',
+        media_type='application/xhtml+xml')
+    nav_item.content = create_toc(title, articles)
+    book.add_item(nav_item)
 
     spine_items = ['nav']  # Initial 'nav' for eBook navigation
-    toc_items = []  # Used for building TOC
 
-    # Iterate over each feed directly from OPML order
-    for feed in feeds_info['feeds']:
-        print(f"Fetching {feed['title']}...")
-        feed_content = fetch_feed_content(feed['xmlUrl'])
-        print(f"Found {len(feed_content)} articles.")
+    # Iterate over the articles dictionary
+    for i, (feed_title, articles) in enumerate(articles.items()):
 
-        # Start the chapter content for the feed
-        feed_chapter_content = f'<h1>{feed["title"]}</h1>'
-        for article in feed_content:
-            # Separate articles by page breaks instead of horizontal rules
-            article_content = f"<div><h2>{article['title']}</h2>"
+        for j, article in enumerate(articles['articles']):
+
+            article_title = article['title']
+            article_content = f'<h1>{article_title}</h1>'
             if 'published' in article:
-                # article_content += f"<p>{article['published']}</p>"
                 article_content += f"<p>{time.strftime('%B %d, %Y, %I:%M %p', article['published'])}</p>"
             if 'author' in article:
                 article_content += f"<p>{article['author']}</p>"
             if 'link' in article:
-                article_content += f"<p>{article['description']}</p>"
                 article_content += f"<p><a href='{article['link']}'>Full Article</a></p>"
-            article_content += "</div><div class='page-break'></div>"
-            feed_chapter_content += article_content
 
-        # Create feed chapter
-        feed_filename = f"{feed['title'].replace(' ', '_').replace('/', '_')}.xhtml"
-        feed_chapter = epub.EpubHtml(title=feed['title'], file_name=feed_filename, lang='en', content=feed_chapter_content)
-        feed_chapter.add_item(nav_css)
-        book.add_item(feed_chapter)
+            # Create a chapter file for each article
+            epub_chapter = epub.EpubHtml(title=article_title, file_name=article['filename'], lang='en')
+            epub_chapter.content = article_content
 
-        # Update spine and TOC
-        spine_items.append(feed_chapter)
-        toc_items.append(feed_chapter)
+            book.add_item(epub_chapter)
+            spine_items.append(epub_chapter)
 
-    # Final book setup
-    book.toc = toc_items
-    book.spine = spine_items
+
+    # Setting the table of contents and spine
+    # book.toc = tuple(toc_feeds)
+
     book.add_item(epub.EpubNcx())
-    book.add_item(epub.EpubNav())
+    # book.add_item(epub.EpubNav())
+    book.spine = spine_items
 
     # Write the EPUB file
-    epub.write_epub(output_path, book, {})
+    epub.write_epub(f"{title}.epub", book, {})
+
+
+def main():
+
+    feeds = read_opml(OPML_PATH)
+    # print(feeds)
+    articles = fetch_feeds(feeds)
+    # print(articles)
+    create_epub(feeds, articles)
+
+
+
+    # output_path = f"{feeds_info['title']} - {datetime.now().strftime('%B %-d, %Y')}.epub"
+    # create_epub_from_opml(feeds_info, output_path)
+    # print(f"EPUB created: {output_path}")
+
 
 # Main execution here
 if __name__ == '__main__':
-    opml_path = 'makepub.opml'
-    feeds_info = read_opml(opml_path)
-    output_path = f"{feeds_info['title']} - {datetime.now().strftime('%B %-d, %Y')}.epub"
-    create_epub_from_opml(feeds_info, output_path)
-    print(f"EPUB created: {output_path}")
+    main()
