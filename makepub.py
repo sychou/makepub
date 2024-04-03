4f67bc09ef9941d1948987594faba37e06211dfa
Sean Chou <sean@choufam.com>
2024-04-03 09:45:41 -0500

    Update nav bugs

--- makepub.py
+++ makepub.py
@@ -71,33 +71,37 @@ def fetch_feeds(opml):
 
     return feeds_content
 
+def create_article_content(article_index, number_articles, article, feed_index, feed_title):
 
-def create_toc(title, articles):
-    """Create a Table of Contents for the EPUB."""
-    # toc = '<nav xmlns:epub="http://www.idpf.org/2007/ops" epub:type="toc">'
-    toc = "<?xml version='1.0' encoding='utf-8'?>\n"
-    toc += '<!DOCTYPE html>\n'
-    toc += '<html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops" lang="en" xml:lang="en">\n'
-    toc += '<head>\n'
-    toc += f"<title>{title}</title>\n"
-    toc += '</head>\n'
-    toc += '<body>\n'
-    toc += '<nav epub:type="toc" id="id" role="doc-toc">\n'
-    toc += '<h2>Table of Contents</h2>\n'
-    toc += '<ol>\n'
-    for feed_title, articles in articles.items():
-        toc += f'<li><a href="{articles["filename"]}">{feed_title}</a></li>\n'
-        toc += '<ol>'
-        for article in articles['articles']:
-            toc += f'<li><a href="{article["filename"]}">{article["title"]}</a></li>\n'
-        toc += '</ol>\n'
-    toc += '</ol>\n'
-    toc += '</nav>\n'
-    toc += '</body>\n'
-    toc += '</html>\n'
+    content = f"<h2>{article['title']}</h2>"
+    if 'published' in article:
+        content += f"<p>{time.strftime('%B %d, %Y, %I:%M %p', article['published'])}</p>"
+    if 'author' in article:
+        content += f"<p>{article['author']}</p>"
+    if 'link' in article:
+        content += f"<p><a href='{article['link']}'>Full Article</a></p>"
 
-    print(toc)
-    return toc
+    # Add navigation markers
+    # Navigation to the previous article
+    if article_index == 1:
+        # If first article of a feed, go to the feed page
+        prev_filename = f'feed_{feed_index}.xhtml'
+    else:
+        prev_filename = f'article_{feed_index}_{article_index - 1}.xhtml'
+
+    content += f"<p><a href='{prev_filename}'>&lt;&lt; Previous</a> | "
+
+    content += f"<a href='nav.xhtml'>{feed_title} ({article_index}/{number_articles})</a>"
+
+    # Navigation to the next article
+    if article_index < number_articles:
+        next_filename = f'article_{feed_index}_{article_index + 1}.xhtml'
+    else:
+        next_filename = f'feed_{feed_index + 1}.xhtml'
+
+    content += f" | <a href='{next_filename}'>Next &gt;&gt;</a></p>"
+
+    return content
 
 
 def create_epub(opml, feeds):
@@ -107,38 +111,28 @@ def create_epub(opml, feeds):
     book = epub.EpubBook()
     book.set_title(title)
 
-    # Create a custom nav.xhtml
-    # nav_item = epub.EpubItem(
-    #     uid='nav',
-    #     file_name='nav.xhtml',
-    #     media_type='application/xhtml+xml')
-    # nav_item.content = create_toc(title, articles)
-    # book.add_item(nav_item)
-
     spine_items = ['nav']  # Initial 'nav' for eBook navigation
     toc_items = []
 
+    number_feeds = len(feeds)
+
     # Iterate over the articles dictionary
-    for i, (feed_title, feed) in enumerate(feeds.items()):
+    for i, (feed_title, feed) in enumerate(feeds.items(), start=1):
 
         # Create the feed content
-        feed_epub = epub.EpubHtml(title=feed_title, file_name=feed['filename'], lang='en')
+        feed_epub = epub.EpubHtml(title=feed_title.upper(), file_name=feed['filename'], lang='en')
         feed_epub.content = f'<h1>{feed_title.upper()}</h1>'
         book.add_item(feed_epub)
         spine_items.append(feed_epub)
         toc_items.append(feed_epub)
 
-        for j, article in enumerate(feed['articles']):
+        number_articles = len(feed['articles'])
+
+        for j, article in enumerate(feed['articles'], start=1):
 
             # Create a chapter file for each article
             article_epub = epub.EpubHtml(title=article['title'], file_name=article['filename'], lang='en')
-            article_epub.content = f"<h2>{article['title']}</h2>"
-            if 'published' in article:
-                article_epub.content += f"<p>{time.strftime('%B %d, %Y, %I:%M %p', article['published'])}</p>"
-            if 'author' in article:
-                article_epub.content += f"<p>{article['author']}</p>"
-            if 'link' in article:
-                article_epub.content += f"<p><a href='{article['link']}'>Full Article</a></p>"
+            article_epub.content = create_article_content(j, number_articles, article, i, feed_title)
 
             # Add to the appropriate lists
             book.add_item(article_epub)
